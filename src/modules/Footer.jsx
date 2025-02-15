2417228e335a4fdd6c2b3e16c76fb108642e3b18
Epsilonxaos <angelotr12@hotmail.com>
2025-02-15 15:56:29 -0600

    feat(Footer): :sparkles: Agregar logo y actualizar diseño del pie de página

--- src/modules/Footer.jsx
+++ src/modules/Footer.jsx
@@ -1,22 +1,50 @@
+import logo from '@assets/img/logo-blanco.svg'
+import Button from '@components/Button'
+import SectionContainer from '@components/SectionContainer'
 import { Link } from 'wouter'
 
 const Footer = () => {
 	return (
-		<footer className='bg-verde py-3 text-center text-gris md:px-[55px]'>
-			<div className='mx-auto flex max-w-[1550px] flex-col md:flex-row md:items-center md:justify-between'>
-				<p>Project</p>
-				<p>
-					Diseño y desarrollo x{' '}
-					<a
-						href='https://www.madeby.partners/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						<span className='font-bold'>MadebyPartners®</span>
-					</a>
-				</p>
-				<Link to='politicas-privacidad'>Políticas de privacidad</Link>
+		<footer className='bg-verde-oscuro text-center text-xs font-thin text-gris'>
+			<SectionContainer className='flex flex-col gap-1 py-3 md:flex-row md:items-center md:justify-between'>
+				<img
+					src={logo}
+					alt='Terreno San ignacio'
+					className='hidden h-[20px] lg:block xl:h-[30px]'
+				/>
+				<a
+					href='/'
+					target='_blank'
+					className='underline'
+					rel='noopener noreferrer'>
+					Ubicación del Predio
+				</a>
+				<a href='tel:+13056131186'>+1 (305) 613-1186</a>
+				<a href='mailto:palfaro@elliman.com'>palfaro@elliman.com</a>
+
+				<div className='hidden md:block'>
+					<ByMBP />
+				</div>
+			</SectionContainer>
+
+			<div className='bg-gris py-2 text-verde-oscuro md:hidden'>
+				<ByMBP />
 			</div>
 		</footer>
+	)
+}
+
+const ByMBP = () => {
+	return (
+		<p className='uppercase'>
+			Desarrollado por{' '}
+			<a
+				href='https://www.madeby.partners/'
+				target='_blank'
+				rel='noopener noreferrer'>
+				<span className='font-normal underline'>MadebyPartners®</span>
+			</a>
+		</p>
 	)
 }
 
